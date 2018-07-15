8e0e3d0adb5aeb00941696e556326c1cbc1f98e2
mdecourse <mdecourse@gmail.com>
2018-07-15 22:00:53 +0800

    fix blog problems

--- pelicanconf.py
+++ pelicanconf.py
@@ -68,7 +68,7 @@ DISPLAY_TAGS_ON_SIDEBAR = True
 DISPLAY_TAGS_INLINE = True
 TAGS_URL = "tags.html"
 CATEGORIES_URL = "categories.html"
-#MENUITEMS = [('About', '/blog/pages/about/')]
+#MENUITEMS = [('About', '/blog/pages/about/index.html')]
 #SHOW_ARTICLE_AUTHOR = True
 
 #MENUITEMS = [('Home', '/'), ('Archives', '/archives.html'), ('Search', '/search.html')]
