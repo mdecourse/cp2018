60faea9614e205536cc7cbf9e6a47f2b78a5d9d9
mdecourse <mdecourse@gmail.com>
2018-08-01 20:30:53 +0800

    update static site title

--- flaskapp.py
+++ flaskapp.py
@@ -1632,7 +1632,7 @@ def set_admin_css():
     outstring = '''<!doctype html>
 <html><head>
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
-<title>CMSimfly</title> \
+<title>計算機程式教材</title> \
 <link rel="stylesheet" type="text/css" href="/static/cmsimply.css">
 ''' + syntaxhighlight()
 
@@ -1681,7 +1681,7 @@ def set_css():
     outstring = '''<!doctype html>
 <html><head>
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
-<title>CMSimfly</title> \
+<title>計算機程式教材</title> \
 <link rel="stylesheet" type="text/css" href="/static/cmsimply.css">
 ''' + syntaxhighlight()
 
@@ -1736,7 +1736,7 @@ def set_css2():
     outstring = '''<!doctype html>
 <html><head>
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
-<title>CMSimfly</title> \
+<title>計算機程式教材</title> \
 <link rel="stylesheet" type="text/css" href="./../static/cmsimply.css">
 ''' + syntaxhighlight2()
 
